2c9e1945648d76356315596b6eaf0eafde548b55
Denys Nazarenko <denysnazarenko23@gmail.com>
2022-06-23 14:23:24 +0300

    start template added

---
+++ app.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import logger from 'morgan';
+import cors from 'cors';
+import { HttpCode } from './lib/constants.js';
+
+import authRouter from './routes/api/auth';
+// import tasksRouter from './routes/api/tasks';
+
+const app = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', authRouter);
+
+app.use((req, res) => {
+    res.status(HttpCode.NOT_FOUND).json({
+        status: 'error',
+        code: HttpCode.NOT_FOUND,
+        message: 'Not found',
+    });
+});
+
+app.use((err, req, res, next) => {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).json({
+        status: 'fail',
+        code: HttpCode.INTERNAL_SERVER_ERROR,
+        message: err.message,
+    });
+});
+
+export default app;
